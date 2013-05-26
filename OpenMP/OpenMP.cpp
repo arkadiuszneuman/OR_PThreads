287268ed3f33d3fa39c77a92a40e0a25c8b9adad
Maciej Płaza <maciejplaza@fastmail.fm>
2013-05-26 12:01:32 +0200

    Refactor Parallel
    Signed-off-by: Maciej Płaza <maciejplaza@fastmail.fm>

--- OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP.cpp
@@ -4,23 +4,26 @@
 #include <omp.h>
 #include <iostream>
 
+// Ustawienie liczby w¹tków.
 #define NUM_THREADS 30
 
 static long num_steps = 10000000000;
 double step;
 
 double Parallel()
-{
-	double sum[NUM_THREADS];
+{	
+	double pi = 0.0;
+	double sum[NUM_THREADS]; // tablica sum z powodu wielodostêpu
 	step = 1.0/(double) num_steps;
 	omp_set_num_threads(NUM_THREADS);
 
 	#pragma omp parallel 
 	{
-		int i;
-		double x;     
-		int id; 
-		id = omp_get_thread_num();
+		int i; // osobne i dla ka¿dego w¹tku z osobna		
+		int id = omp_get_thread_num(); // numer w¹tku
+		double x; // zmienna pomocnicza
+
+		// g³ówna pêtla
 		for (i = id, sum[id] = 0.0; i < num_steps; i = i + NUM_THREADS)
 		{ 
 			x = (i+0.5)*step; 
@@ -28,19 +31,21 @@ double Parallel()
 		} 
 	}
 
-	int i;
-	double pi;
-	for(i=0, pi=0.0; i<NUM_THREADS; i++)
+	// podliczanie wyników cz¹stkowych
+	for(int i = 0; i < NUM_THREADS; i++) 
+	{
 		pi += sum[i] * step; 
+	}
 
 	return pi;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	double pi = Parallel();
+	// Wyœwietlanie wyników.
 	std::cout.precision(50);
-	std::cout << pi << std::endl;
+	std::cout << "Parallel SPMD" << Parallel() << std::endl;
+	
 	_getch();
 
 	return 0;
