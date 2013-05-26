79acedcb41444aaf609b6def7d15e3aeda9b9e28
Maciej Płaza <maciejplaza@fastmail.fm>
2013-05-26 11:50:48 +0200

    OpenMP Parallel
    Signed-off-by: Maciej Płaza <maciejplaza@fastmail.fm>

--- OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP.cpp
@@ -4,36 +4,34 @@
 #include <omp.h>
 #include <iostream>
 
-static long num_steps = 1000000;
+#define NUM_THREADS 30
+
+static long num_steps = 10000000000;
 double step;
 
 double Parallel()
 {
-	int i;
-	int threads = omp_get_num_threads();
-	double x, pi;
-	double* sum = new double[threads];
-	for (int i = 0; i < threads; ++i)
-	{
-		sum[i] = 0;
-	}
+	double sum[NUM_THREADS];
 	step = 1.0/(double) num_steps;
+	omp_set_num_threads(NUM_THREADS);
 
-#pragma omp parallel 
+	#pragma omp parallel 
 	{
-		double x;     int id; 
+		int i;
+		double x;     
+		int id; 
 		id = omp_get_thread_num();
-		for (i=id, sum[id]=0.0;i< num_steps; i=i+threads){ 
+		for (i = id, sum[id] = 0.0; i < num_steps; i = i + NUM_THREADS)
+		{ 
 			x = (i+0.5)*step; 
 			sum[id] += 4.0/(1.0+x*x); 
 		} 
 	}
 
-	for(i=0, pi=0.0;i<threads;i++)
+	int i;
+	double pi;
+	for(i=0, pi=0.0; i<NUM_THREADS; i++)
 		pi += sum[i] * step; 
-
-	//delete[] sum;
-	//sum = NULL;
 
 	return pi;
 }
@@ -41,7 +39,7 @@ double Parallel()
 int _tmain(int argc, _TCHAR* argv[])
 {
 	double pi = Parallel();
-
+	std::cout.precision(50);
 	std::cout << pi << std::endl;
 	_getch();
 
