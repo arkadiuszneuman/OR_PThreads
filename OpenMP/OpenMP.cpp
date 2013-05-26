b525166265ff41ed172587625b57875e8d060eb7
Arkadiusz Neuman <mrmoro@o2.pl>
2013-05-26 11:27:59 +0200

    OpenMP - start.....

--- OpenMP/OpenMP.cpp
+++ OpenMP/OpenMP.cpp
@@ -2,24 +2,47 @@
 #include "stdafx.h"
 #include <conio.h>
 #include <omp.h>
+#include <iostream>
 
-//#pragma omp 
-
-static long num_steps = 100000;
+static long num_steps = 1000000;
 double step;
+
+double Parallel()
+{
+	int i;
+	int threads = omp_get_num_threads();
+	double x, pi;
+	double* sum = new double[threads];
+	for (int i = 0; i < threads; ++i)
+	{
+		sum[i] = 0;
+	}
+	step = 1.0/(double) num_steps;
+
+#pragma omp parallel 
+	{
+		double x;     int id; 
+		id = omp_get_thread_num();
+		for (i=id, sum[id]=0.0;i< num_steps; i=i+threads){ 
+			x = (i+0.5)*step; 
+			sum[id] += 4.0/(1.0+x*x); 
+		} 
+	}
+
+	for(i=0, pi=0.0;i<threads;i++)
+		pi += sum[i] * step; 
+
+	//delete[] sum;
+	//sum = NULL;
+
+	return pi;
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int i;
-	double x, pi, sum = 0.0;
-	step = 1.0/(double) num_steps;
-	for (i=1; i<= num_steps; i++)
-	{
-		x = (i-0.5)*step;
-		sum = sum + 4.0/(1.0+x*x);
-	}
-	pi = step * sum;
+	double pi = Parallel();
 
+	std::cout << pi << std::endl;
 	_getch();
 
 	return 0;
